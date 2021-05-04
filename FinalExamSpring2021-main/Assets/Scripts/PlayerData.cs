b168a96f02f2066aca769be32aab71be59a173b6
Lane <lanec92@gmail.com>
2021-05-04 15:37:31 -0500

    Update PlayerData.cs

--- FinalExamSpring2021-main/Assets/Scripts/PlayerData.cs
+++ FinalExamSpring2021-main/Assets/Scripts/PlayerData.cs
@@ -6,8 +6,6 @@ public class PlayerData
     public int currScore;
     public int currLives;
     public int currTime;
-    public string currName;
-
     public PlayerData(GameManager gm)
     {
         currLives = PickLives.playerLives;
